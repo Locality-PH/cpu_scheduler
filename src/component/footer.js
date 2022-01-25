6eca59c7627ffee4c8f16a2361e73063182a4da2
vonypeto <von.aralar@gmail.com>
2022-01-25 09:30:41 +0800

    modified footer

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import { Layout } from 'antd';
+import React from "react";
+import { Layout } from "antd";
 
-
-const {  Footer } = Layout;
+const { Footer } = Layout;
 const footer = () => {
-    return (
-        <div>
-               <Footer style={{ textAlign: 'center' }}>CPU Scheduler 2021 Created by Team BaranGAY</Footer>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Footer style={{ textAlign: "center" }}>
+        CPU Scheduler 2021 Created by Team Locality
+      </Footer>
+    </div>
+  );
+};
 
-export default footer
+export default footer;
